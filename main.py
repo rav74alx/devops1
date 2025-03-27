e6a9672d1a4f5756f7056a1cb48a52404c8cb568
rav <kurs@DESKTOP-V3HDKPQ.com>
2025-03-27 10:50:04 +0100

    cherry opis zmiany...

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 print("pierwsza linia kodu")
 print("druga linia kodu")
+<<<<<<< HEAD
 print("trzecia linia kodu")
 print("6 linia")
 
 print("czwarta linia kodu")
+=======
+print("trzecia linia kodu")
+>>>>>>> ac336d0 (opis zmiany...)
