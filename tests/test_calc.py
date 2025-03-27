65a463641376cb4d406ad651ef0b7ddbb94d2562
rav <kurs@DESKTOP-V3HDKPQ.com>
2025-03-27 16:36:57 +0100

    test3

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,8 +1,20 @@
+import pytest
+
 def add(a,b):
     return a + b
 
 def test_add():
     assert add(2,3) == 5
 
-def test_add():
-    assert add(2,3) == 55
+# def test_add():
+#     assert add(2,3) == 55
+
+@pytest.mark.parametrize("a,b,wynik",[
+    (2,3,5),
+    (3,5,12),
+    (22,33,12)
+])
+
+def test_param_add(a,b,wynik)
+    assert add(ab) == wynik
+    
